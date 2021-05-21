86c306d68eb9a243726097f9b72c7525561a9eff
Dings <deen.job@qq.com>
2021-05-21 14:22:33 +0800

    updated: linked list.

--- list/linked_list.go
+++ list/linked_list.go
@@ -5,8 +5,8 @@ import "errors"
 
 // Node 是链表的节点
 type Node struct {
-	value interface{} `json:'value'`
-	next  *Node       `json:'next_node'`
+	Value interface{} `json:"value"`
+	Next  *Node       `json:"next_node"`
 }
 
 // List 链表的抽象接口
@@ -36,25 +36,25 @@ func (list *LinkedList) Insertion(index int, value interface{}) error {
 	if index < 0 || index > list.size {
 		return errors.New("index out of bounds")
 	}
-	node := &Node{value: value, next: nil}
+	node := &Node{Value: value, Next: nil}
 	if list.size == 0 {
 		// 空链表
 		list.head = node
 		list.last = node
 	} else if index == 0 {
 		// 追加插入
-		node.next = list.head
+		node.Next = list.head
 		list.head = node
 	} else if list.size == index {
-		list.last.next = node
+		list.last.Next = node
 		list.last = node
 	} else {
 		// 找到3个元素中前面的那个节点
 		perv := list.Get(index - 1)
 		// 把插入节点的next指向原理位置的下一个那块next
-		node.next = perv.next
+		node.Next = perv.Next
 		// 前面next指向插入的
-		perv.next = node
+		perv.Next = node
 	}
 	list.size++
 	return nil
@@ -63,7 +63,7 @@ func (list *LinkedList) Insertion(index int, value interface{}) error {
 func (list *LinkedList) Get(index int) *Node {
 	node := list.head
 	for i := 0; i < index; i++ {
-		node = node.next
+		node = node.Next
 	}
 	return node
 }
@@ -71,7 +71,7 @@ func (list *LinkedList) Get(index int) *Node {
 func (list *LinkedList) Remove(index int) {
 
 	if index == 0 {
-		next := list.head.next
+		next := list.head.Next
 		list.head = next
 	} else if index == list.size {
 		perv := list.Get(list.size - 1)
@@ -79,8 +79,8 @@ func (list *LinkedList) Remove(index int) {
 	} else {
 		perv := list.Get(index - 1)
 		// 移除中间的那个
-		next := perv.next.next
-		perv.next = next
+		next := perv.Next.Next
+		perv.Next = next
 	}
 	list.size--
 
@@ -91,19 +91,19 @@ func (list *LinkedList) Range(channel chan *Node) {
 	go func() {
 		for node != nil {
 			channel <- node
-			node = node.next
+			node = node.Next
 		}
 		close(channel)
 	}()
 }
 
 func (list *LinkedList) Add(value interface{}) {
-	node := &Node{value: value}
+	node := &Node{Value: value}
 	if list.size == 0 {
 		list.head = node
 		list.last = node
 	} else {
-		list.last.next = node
+		list.last.Next = node
 		list.last = node
 	}
 	list.size++
