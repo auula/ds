402df400aafa568de06f63a0f71971e51f55c1b2
Dings <deen.job@qq.com>
2021-05-29 18:30:04 +0800

    add:CMap ConcurrentMap.

--- cmap/cmap.go
+++ cmap/cmap.go
@@ -11,42 +11,71 @@ import (
 	"sync"
 )
 
-type blockMap []*sync.Map
+// 分段map集合
+type blockMaps []*blockMap
+
+// 每块map里面存储数据段
+type mapItem map[interface{}]interface{}
 
 type ConcurrentMap struct {
-	blockMap     // 分段的同步map块
-	size     int // 有多少个块
+	blockMaps     // 分段的同步map块
+	size      int // 有多少个块
+}
+
+type blockMap struct {
+	// 分段的map块
+	items mapItem
+	sync.RWMutex
+}
+
+func (m *blockMap) set(k, v interface{}) {
+	m.Lock()
+	m.items[k] = v
+	m.Unlock()
+}
+
+func (m *blockMap) get(k interface{}) interface{} {
+	m.RLock()
+	v := m.items[k]
+	m.RUnlock()
+	return v
 }
 
 func New(size int) *ConcurrentMap {
 	c := new(ConcurrentMap)
 	c.size = size
-	c.blockMap = make(blockMap, size)
+	c.blockMaps = make(blockMaps, size)
 	for i := 0; i < c.size; i++ {
-		c.blockMap[i] = &sync.Map{}
+		c.blockMaps[i] = &blockMap{
+			items: make(mapItem),
+		}
 	}
 	return c
 }
 
-func (m *ConcurrentMap) GetMap(key interface{}) *sync.Map {
+func (m *ConcurrentMap) blockMap(key interface{}) *blockMap {
 	// 通过哈希计算得到map的所在的位置
-	return m.blockMap[(m.Hash(key) % m.size)]
+	return m.blockMaps[(m.HashCode(key) % m.size)]
 }
 
 func (m *ConcurrentMap) Set(k, v interface{}) {
-	smap := m.GetMap(k)
-	smap.Store(k, v)
+	b := m.blockMap(k)
+	b.set(k, v)
 }
 
 func (m *ConcurrentMap) Get(k interface{}) interface{} {
-	smap := m.GetMap(k)
-	if v, ok := smap.Load(k); ok {
-		return v
-	}
-	return nil
+	b := m.blockMap(k)
+	return b.get(k)
 }
 
-func (m *ConcurrentMap) Hash(key interface{}) (code int) {
+func (m *ConcurrentMap) Remove(k interface{}) {
+	b := m.blockMap(k)
+	b.Lock()
+	delete(b.items, k)
+	b.Unlock()
+}
+
+func (m *ConcurrentMap) HashCode(key interface{}) (code int) {
 	switch key.(type) {
 	case string:
 		code = _stringToCode(key.(string))
--- cmap/cmap_test.go
+++ cmap/cmap_test.go
@@ -7,22 +7,174 @@
 package cmap
 
 import (
-	"crypto/rand"
+	crand "crypto/rand"
 	"encoding/binary"
 	"fmt"
+	"math/rand"
 	"strconv"
 	"sync"
 	"testing"
 )
 
+var N = 10
+
+var simpleMap map[string]interface{}
+var mutexMap MutexMap
+var syncMap sync.Map
+var concurrentMap *ConcurrentMap
+
+type MutexMap struct {
+	sync.RWMutex
+	m map[string]interface{}
+}
+
+func (m *MutexMap) Set(k string, v interface{}) {
+	m.Lock()
+	m.m[k] = v
+	m.Unlock()
+}
+
+func (m *MutexMap) Get(k string) (v interface{}) {
+	m.RLock()
+	v = m.m[k]
+	m.RUnlock()
+	return
+}
+
+func init() {
+	simpleMap = make(map[string]interface{}, N)
+	mutexMap = MutexMap{m: make(map[string]interface{}, N)}
+	syncMap = sync.Map{}
+	for i := 0; i < N; i++ {
+		syncMap.Store(strconv.Itoa(i), i)
+	}
+	concurrentMap = New(32)
+}
+
+func BenchmarkMap_Set(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wg := sync.WaitGroup{}
+		wg.Add(N)
+		for j := 0; j < N; j++ {
+			idx := rand.Intn(N)
+			simpleMap[strconv.Itoa(idx)] = j + 1
+			wg.Done()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkMap_Get(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wg := sync.WaitGroup{}
+		wg.Add(N)
+		for j := 0; j < N; j++ {
+			idx := rand.Intn(N)
+			_ = simpleMap[strconv.Itoa(idx)]
+			wg.Done()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkMutexMap_Set(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wg := sync.WaitGroup{}
+		wg.Add(N)
+		for j := 0; j < N; j++ {
+			go func() {
+				idx := rand.Intn(N)
+				mutexMap.Set(strconv.Itoa(idx), j+1)
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkMutexMap_Get(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wg := sync.WaitGroup{}
+		wg.Add(N)
+		for j := 0; j < N; j++ {
+			go func() {
+				idx := rand.Intn(N)
+				mutexMap.Get(strconv.Itoa(idx))
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkSyncMap_Set(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wg := sync.WaitGroup{}
+		wg.Add(N)
+		for j := 0; j < N; j++ {
+			go func() {
+				idx := rand.Intn(N)
+				syncMap.Store(strconv.Itoa(idx), j+1)
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkSyncMap_Get(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wg := sync.WaitGroup{}
+		wg.Add(N)
+		for j := 0; j < N; j++ {
+			go func() {
+				idx := rand.Intn(N)
+				syncMap.Load(strconv.Itoa(idx))
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkCMap_Set(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wg := sync.WaitGroup{}
+		wg.Add(N)
+		for j := 0; j < N; j++ {
+			go func() {
+				idx := rand.Intn(N)
+				concurrentMap.Set(strconv.Itoa(idx), j+1)
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkCMap_Get(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wg := sync.WaitGroup{}
+		wg.Add(N)
+		for j := 0; j < N; j++ {
+			go func() {
+				idx := rand.Intn(N)
+				concurrentMap.Get(strconv.Itoa(idx))
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
 func BenchmarkConcurrentMap(b *testing.B) {
 	concurrentMap := New(32 * 2)
-	nums := 100000
+	nums := 10000
 	b.ResetTimer()
 
 	for i := 0; i < 5; i++ {
 		b.Run(strconv.Itoa(i), func(b *testing.B) {
-			b.N = 10000000
+			b.N = 1000000
 			wg := sync.WaitGroup{}
 			wg.Add(b.N * 2)
 			for i := 0; i < b.N; i++ {
@@ -44,9 +196,38 @@ func BenchmarkConcurrentMap(b *testing.B) {
 	}
 }
 
+func BenchmarkSyncMap(b *testing.B) {
+	m := sync.Map{}
+	nums := 10000
+	b.ResetTimer()
+
+	for i := 0; i < 5; i++ {
+		b.Run(strconv.Itoa(i), func(b *testing.B) {
+			b.N = 1000000
+			wg := sync.WaitGroup{}
+			wg.Add(b.N * 2)
+			for i := 0; i < b.N; i++ {
+				// 模拟并发随机写
+				go func(key string, val interface{}) {
+					m.Store(key, val)
+					wg.Done()
+				}(fmt.Sprintf("k%d", _randomInt(nums)), _randomInt(nums))
+
+				// // 模拟并发随机读
+				go func(key string) {
+					m.Load(key)
+					wg.Done()
+				}(fmt.Sprintf("k%d", _randomInt(nums)))
+
+			}
+			wg.Wait()
+		})
+	}
+}
+
 func _randomInt(max int) int {
 	var n uint16
-	binary.Read(rand.Reader, binary.LittleEndian, &n)
+	binary.Read(crand.Reader, binary.LittleEndian, &n)
 	return int(n) % max
 
 }
