5b9b42d2d119058424f82cc340381d92a3fd20c6
Dings <deen.job@qq.com>
2021-06-10 20:23:04 +0800

    add:  min heap.

--- heap/min_heap.go
+++ heap/min_heap.go
@@ -13,64 +13,64 @@ func New() *MinHeap {
 	return &MinHeap{Element: []int{math.MinInt64}}
 }
 
-func (mh *MinHeap) Build(values ...int) {
+func (m *MinHeap) Build(values ...int) {
 	for _, v := range values {
-		mh.Insert(v)
+		m.Insert(v)
 	}
 }
 
 // 插入数字,插入数字需要保证堆的性质
-func (H *MinHeap) Insert(v int) {
-	H.Element = append(H.Element, v)
-	i := len(H.Element) - 1
+func (m *MinHeap) Insert(v int) {
+	m.Element = append(m.Element, v)
+	i := len(m.Element) - 1
 	// 上浮
-	for ; H.Element[i/2] > v; i /= 2 {
-		H.Element[i] = H.Element[i/2]
+	for ; m.Element[i/2] > v; i /= 2 {
+		m.Element[i] = m.Element[i/2]
 	}
-	H.Element[i] = v
+	m.Element[i] = v
 }
 
 // 删除并返回最小值
-func (H *MinHeap) DeleteMin() (int, error) {
-	if len(H.Element) <= 1 {
+func (m *MinHeap) DeleteMin() (int, error) {
+	if len(m.Element) <= 1 {
 		return 0, fmt.Errorf("MinHeap is empty")
 	}
-	minElement := H.Element[1]
-	lastElement := H.Element[len(H.Element)-1]
+	minElement := m.Element[1]
+	lastElement := m.Element[len(m.Element)-1]
 	var i, child int
-	for i = 1; i*2 < len(H.Element); i = child {
+	for i = 1; i*2 < len(m.Element); i = child {
 		child = i * 2
-		if child < len(H.Element)-1 && H.Element[child+1] < H.Element[child] {
+		if child < len(m.Element)-1 && m.Element[child+1] < m.Element[child] {
 			child++
 		}
 		// 下滤一层
-		if lastElement > H.Element[child] {
-			H.Element[i] = H.Element[child]
+		if lastElement > m.Element[child] {
+			m.Element[i] = m.Element[child]
 		} else {
 			break
 		}
 	}
-	H.Element[i] = lastElement
-	H.Element = H.Element[:len(H.Element)-1]
+	m.Element[i] = lastElement
+	m.Element = m.Element[:len(m.Element)-1]
 	return minElement, nil
 }
 
 // 堆的大小
-func (H *MinHeap) Length() int {
-	return len(H.Element) - 1
+func (m *MinHeap) Length() int {
+	return len(m.Element) - 1
 }
 
 // 获取最小堆的最小值
-func (H *MinHeap) Min() (int, error) {
-	if len(H.Element) > 1 {
-		return H.Element[1], nil
+func (m *MinHeap) Min() (int, error) {
+	if len(m.Element) > 1 {
+		return m.Element[1], nil
 	}
 	return 0, fmt.Errorf("heap is empty")
 }
 
 // MinHeap格式化输出
-func (H *MinHeap) String() string {
-	return fmt.Sprintf("%v", H.Element[1:])
+func (m *MinHeap) String() string {
+	return fmt.Sprintf("%v", m.Element[1:])
 }
 
 func main() {
