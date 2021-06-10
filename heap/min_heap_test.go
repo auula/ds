c11a4fd14b267475713a61c51ee7379dff973deb
Dings <deen.job@qq.com>
2021-06-10 23:41:48 +0800

    add: TestMinHeap

--- heap/min_heap_test.go
+++ heap/min_heap_test.go
@@ -6,6 +6,8 @@ import (
 
 func TestMinHeap(t *testing.T) {
 	heap := New()
-	heap.Build(11, 2, 22, 3, -2, 43, 21, 18)
-	t.Log(heap.Min())
+	heap.Build(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -1)
+	t.Log(heap.DeleteMin())
+	t.Log(heap.DeleteMin())
+	t.Log(heap.DeleteMin())
 }
