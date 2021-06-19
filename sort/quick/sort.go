8d28d3c70b28fb98440555506fd591a378645604
Dings <deen.job@qq.com>
2021-06-19 20:35:54 +0800

    add:quick sort.

---
+++ sort/quick/sort.go
@@ -0,0 +1,48 @@
+package quick
+
+import "github.com/higker/ds/sort"
+
+type (
+	moreThan struct{}
+	lessThan struct{}
+)
+
+func New() sort.Sorting {
+	return &moreThan{}
+}
+
+// 快速排序
+// 右小停，左大停，然后交换左右
+// 大循环结束 移动重合指针的元素
+func partition(arr []float64, startIndex, endIndex int) int {
+	left, right := startIndex, endIndex
+	pivot := arr[startIndex]
+	for left != right {
+		for left < right && arr[right] > pivot {
+			right--
+		}
+		for left < right && arr[left] < pivot {
+			left++
+		}
+		if left < right {
+			arr[left] = arr[right]
+		}
+	}
+	arr[startIndex] = arr[left]
+	arr[left] = pivot
+	return left
+}
+
+func quickSort(arr []float64, startIndex, endIndex int) {
+	if startIndex >= endIndex {
+		return
+	}
+	// 第一次循环区分中线，然后左右递归
+	pivotIndex := partition(arr, startIndex, endIndex)
+	quickSort(arr, startIndex, pivotIndex-1)
+	quickSort(arr, pivotIndex+1, endIndex)
+}
+
+func (m *moreThan) Sort(arr []float64) {
+	quickSort(arr, 0, len(arr)-1)
+}
---
+++ sort/quick/sort_test.go
@@ -0,0 +1,13 @@
+package quick_test
+
+import (
+	"github.com/higker/ds/sort/quick"
+	"testing"
+)
+
+func TestQuickSort(t *testing.T) {
+	qs := quick.New()
+	arrays := []float64{12, 322, 123.1, 1, 3, 4}
+	qs.Sort(arrays)
+	t.Log(arrays)
+}
