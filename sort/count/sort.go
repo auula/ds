fab688dab0f4b4e821e91714502f17a577b7f058
Dings <deen.job@qq.com>
2021-06-26 22:20:07 +0800

    modify: edit annotation.

--- sort/count/sort.go
+++ sort/count/sort.go
@@ -4,16 +4,21 @@ func Sort(arr []int) {
 	copy(arr, sorting(arr))
 }
 
+// 为不浪费空间 数组每个元素是某一个范围内 有点大材小用
+// 下标计算就需要偏移量了min
 func sorting(arr []int) []int {
 	// 计算出数列的最大长度
-	max := arr[0]
+	max, min := arr[0], arr[0]
 	for i := 1; i < len(arr); i++ {
 		if arr[i] > max {
 			max = arr[i]
 		}
+		if arr[i] > min {
+			min = arr[i]
+		}
 	}
-	// 统计出现的次数
-	countArrays := make([]int, max+1)
+	// 统计出现的次数 max-min+1
+	countArrays := make([]int, max-min+1)
 	for i := 0; i < len(arr); i++ {
 		countArrays[arr[i]]++
 	}
